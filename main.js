129362e00ff2c3e4e144c15ae121002797861f53
cvanama <cvanama@gmail.com>
2023-04-17 17:52:59 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+noseX = 0;
+noseY = 0;
+
+function preload(){
+clown_nose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();  
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log("PoseNet Is Initialized");
+}
+
+function gotPoses(results){
+    if (results.length > 0) {
+        
+
+        console.log(results);
+
+         noseX = results[0].pose.nose.x;
+         noseY = results[0].pose.nose.y;
+         console.log("nose x =" + noseX);
+         console.log("nose y =" + noseY);
+         
+        console.log("nose x ="+results[0].pose.nose.x);
+        console.log("nose y ="+results[0].pose.nose.y);
+
+
+    }
+}
+
+function draw(){
+   image(video ,0,0,300,300)
+   fill(255,0,0);
+   stroke(255,0,0);
+   image(clown_nose , noseX - 10 , noseY - 10 , 25 , 25)
+}
+
+function take_snapshot(){
+    save('myFillterImage.png');
+}
